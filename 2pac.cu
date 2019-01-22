8cc045d7df856d7ebdf16f7e2c4b30ea697f650b
hilinden <hilding.linden@abo.fi>
2019-01-22 18:31:11 +0200

    Refactor to only use single-precision and only on device

--- 2pac.cu
+++ 2pac.cu
@@ -7,56 +7,62 @@
 
 // 2-point angular correlation
 
+const int BLOCKSIZE = 256;
 const int ROWSPERTHREAD = 256;
-const double RR_DEVICE_TO_HOST_RATIO = 0.90;
 
-// Single-precision computation (is the precision enough?)
-__global__ void DR_kernel(int nCols, int nRows, float *D, double *R, int *DR) {
+// Columns are D and rows are R
+__global__ void DR_kernel(int nCols, int nRows, float *D, float *R, unsigned long long int *gHist) {
 
 	// The thread id on the x-axis and y-axis
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	int y = blockIdx.y * ROWSPERTHREAD;
 
-	// blockIdx.y * ROWSPERTHREAD
-
+	// If the thread is inside the domain
 	if (x < nCols) {
 
 		// Shared histogram for the thread block
-		__shared__ int hist[720];
+		__shared__ unsigned int sHist[720];
 
 		// Thread number zero will initialize the shared memory
 		if (threadIdx.x == 0) {
 			for (int i = 0; i < 720; i++) {
-				hist[i] = 0;
+				sHist[i] = 0;
 			}
 		}
 
 		__syncthreads();
 
-		//__shared__ double sharedR[ROWSPERTHREAD * 2 + 1];
-
-		// Right ascension of R
-		//sharedR[threadIdx.x * 2] = r[(y + threadIdx.x) * 2];
-		// Declination of R
-		//sharedR[threadIdx.x * 2 + 1] = r[(y + threadIdx.x) * 2 + 1];
-
-		// Right ascension and declination for the current element in D
+		// Right ascension and declination in arc minutes for the current column
 		float asc1 = D[x * 2];
 		float dec1 = D[x * 2 + 1];
 
-		// Each thread calculates ROWSPERTHREAD elements of R or however many there are left to calculate
+		// Alpha and delta
+		float alpha1 = asc1;
+		float delta1 = 90 - dec1;
+
+		// The amount of rows to be calculated is ROWSPERTHREAD or rows left in the domain, whichever is smaller
 		int nElements = min(nRows-y, ROWSPERTHREAD);
-		float decimalResult;
-
+		
 		for (int j = 0; j < nElements; j++) {
-			double asc2 = R[y + j * 2];
-			double dec2 = R[y + j * 2 + 1];
+			// Right ascension and declination in arc minutes for the current row
+			float asc2 = R[y + j * 2];
+			float dec2 = R[y + j * 2 + 1];
 
-			if (fabs((double)asc1-asc2) > 0.0000001 || fabs((double)dec1-dec2) > 0.0000001) {
-				decimalResult = acosf(sinf(dec1) * sinf(dec2) + cosf(dec1) * cosf(dec2) * cosf(asc1-(float)asc2));
-				decimalResult *= 180/M_PI;
-				int resultIndex = floor(decimalResult/0.25);
-				atomicAdd(&hist[resultIndex], 1);
+			// Alpha and delta
+			float alpha2 = asc2;
+			float delta2 = 90 - dec2;
+
+			// Check if the coordinates are identical
+			if (fabsf(alpha1 - alpha2) > 0.0000001f || fabsf(delta1 - delta2) > 0.0000001f) {
+				// Compute the angle
+				float decimalResult = acosf(sinf(delta1) * sinf(delta2) + cosf(delta1) * cosf(delta2) * cosf(alpha1-alpha2));
+				// Compute the bin
+				int resultIndex = floor(decimalResult/0.25f);
+				// Increment the bin in the shared histogram
+				atomicAdd(&sHist[resultIndex], 1);
+			} else {
+				//printf("Same coordinates in DR\n");
+				atomicAdd(&sHist[0], 1);
 			}
 		}
 
@@ -65,53 +71,71 @@ __global__ void DR_kernel(int nCols, int nRows, float *D, double *R, int *DR) {
 		// Thread number zero will write the shared histogram to global device memory
 		if (threadIdx.x == 0) {
 			for (int i = 0; i < 720; i++) {
-				atomicAdd(&DR[i], hist[i]);
+				// Update the global histogram with the shared histogram
+				atomicAdd(&gHist[i], sHist[i]);
 			}
 		}
 	}
 }
 
 // All computation in single-precision
-__global__ void DD_kernel(int n, float *D, int *DD) {
+__global__ void DD_or_RR_kernel(int nCols, int nRows, float *arr, unsigned long long int *gHist) {
 
 	// The thread id on the x-axis and y-axis
 	//int x = blockIdx.y * ROWSPERTHREAD + blockIdx.x * blockDim.x + threadIdx.x;
 	int x = blockIdx.x * blockDim.x + threadIdx.x;
 	int y = blockIdx.y * ROWSPERTHREAD;
 
-	if (x < n && y + ROWSPERTHREAD > x) {
+	// If the column is inside the domain and the last row of the thread should be computed
+	if (x < nCols && y + ROWSPERTHREAD > x) {
 
 		// Shared histogram for the thread block
-		__shared__ int hist[720];
+		__shared__ unsigned int sHist[720];
 
 		// Thread number zero will initialize the shared memory
 		if (threadIdx.x == 0) {
 			for (int i = 0; i < 720; i++) {
-				hist[i] = 0;
+				sHist[i] = 0;
 			}
 		}
 
 		__syncthreads();
 
-		// Right ascension and declination for the current element in D
-		float asc1 = D[x * 2];
-		float dec1 = D[x * 2 + 1];
+		// Right ascension and declination in arc minutes for the current column
+		float asc1 = arr[x * 2];
+		float dec1 = arr[x * 2 + 1];
 
+		// Alpha and delta
+		float alpha1 = asc1;
+		float delta1 = 90 - dec1;
+
+		// Offset is at which row to start computing
 		int offset = max(x-y+1, 0);
 
-		// Each thread calculates ROWSPERTHREAD elements of D or however many there are left to calculate
-		int nElements = min(n-y, ROWSPERTHREAD);
-
-		float decimalResult;
+		// The amount of rows to be calculated is ROWSPERTHREAD or rows left in the domain, whichever is smaller
+		int nElements = min(nRows-y, ROWSPERTHREAD);
+		
 		for (int j = offset; j < nElements; j++) {
-			float asc2 = D[(y + j) * 2];
-			float dec2 = D[(y + j) * 2 + 1];
+			// Right ascension and declination in arc minutes for the current row
+			float asc2 = arr[y + j * 2];
+			float dec2 = arr[y + j * 2 + 1];
 
-			if (fabs(asc1-asc2) > 0.0000001 || fabs(dec1-dec2) > 0.0000001) {
-				decimalResult = acosf(sinf(dec1) * sinf(dec2) + cosf(dec1) * cosf(dec2) * cosf(asc1-asc2));
-				decimalResult *= 180/M_PI;
-				int resultIndex = floor(decimalResult/0.25);
-				atomicAdd(&hist[resultIndex], 1);
+			// Alpha and delta
+			float alpha2 = asc2;
+			float delta2 = 90 - dec2;
+
+			// Check if the coordinates are identical
+			if (fabsf(alpha1 - alpha2) > 0.0000001f || fabsf(delta1 - delta2) > 0.0000001f) {
+				// Compute the angle
+				float decimalResult = acosf(sinf(delta1) * sinf(delta2) + cosf(delta1) * cosf(delta2) * cosf(alpha1-alpha2));
+				// Compute the bin
+				int resultIndex = floor(decimalResult/0.25f);
+				// Increment the bin in the shared histogram
+				atomicAdd(&sHist[resultIndex], 2);
+			} else {
+				//printf("Same coordinates in DD or RR\n");
+				// Add two we compute the angle between a pair only once
+				atomicAdd(&sHist[0], 2);
 			}
 		}
 
@@ -120,62 +144,8 @@ __global__ void DD_kernel(int n, float *D, int *DD) {
 		// Thread number zero will write the shared histogram to global device memory
 		if (threadIdx.x == 0) {
 			for (int i = 0; i < 720; i++) {
-				atomicAdd(&DD[i], hist[i]);
-			}
-		}
-	}
-}
-
-// All computation in double-precision
-__global__ void RR_kernel(int xElements, int yElements, double *R, int *RR) {
-
-	// The thread id on the x-axis and y-axis
-	//int x = blockIdx.y * ROWSPERTHREAD + blockIdx.x * blockDim.x + threadIdx.x;
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	int y = blockIdx.y * ROWSPERTHREAD;
-
-	if (x < xElements && y + ROWSPERTHREAD > x) {
-
-		// Shared histogram for the thread block
-		__shared__ int hist[720];
-
-		// Thread number zero will initialize the shared memory
-		if (threadIdx.x == 0) {
-			for (int i = 0; i < 720; i++) {
-				hist[i] = 0;
-			}
-		}
-
-		__syncthreads();
-
-		// Right ascension and declination for the current element in D
-		double asc1 = R[x * 2];
-		double dec1 = R[x * 2 + 1];
-
-		int offset = max(x-y+1, 0);
-
-		// Each thread calculates ROWSPERTHREAD elements of D or however many there are left to calculate
-		int nElements = min(yElements-y, ROWSPERTHREAD);
-
-		double decimalResult;
-		for (int j = offset; j < nElements; j++) {
-			double asc2 = R[(y + j) * 2];
-			double dec2 = R[(y + j) * 2 + 1];
-
-			if (fabs(asc1-asc2) > 0.0000001 || fabs(dec1-dec2) > 0.0000001) {
-				decimalResult = acos(sin(dec1) * sin(dec2) + cos(dec1) * cos(dec2) * cos(asc1-asc2));
-				decimalResult *= 180/M_PI;
-				int resultIndex = floor(decimalResult/0.25);
-				atomicAdd(&hist[resultIndex], 1);
-			}
-		}
-
-		__syncthreads();
-
-		// Thread number zero will write the shared histogram to global device memory
-		if (threadIdx.x == 0) {
-			for (int i = 0; i < 720; i++) {
-				atomicAdd(&RR[i], hist[i]);
+				// Update the global histogram with the shared histogram
+				atomicAdd(&gHist[i], sHist[i]);
 			}
 		}
 	}
@@ -203,150 +173,98 @@ int main(void) {
 	printf("Found %d coordinate pairs in flat\n", nCoordinatePairsR);
 
 	// Allocate memory for synthetic data on host
-	double *h_R = (double *)malloc(nCoordinatePairsR * 2 * sizeof(double));
+	float *h_R = (float *)malloc(nCoordinatePairsR * 2 * sizeof(float));
 
 	if (h_D == NULL || h_R == NULL) printf("Allocating memory on host failed");
 
-
 	int index = 0;
 
-	// Initialize data
+	// Read, convert from arc minutes to degrees and store in D
 	float ascD, decD;
 	while (infileD >> ascD >> decD) {
 		if (index < nCoordinatePairsD * 2) {
-			h_D[index++] = ascD;
-			h_D[index++] = decD;
+			h_D[index++] = ascD * 1/60 * M_PI/180;
+			h_D[index++] = decD * 1/60 * M_PI/180;
 		} else {
 			printf("Number of coordinate pairs given in file does not match the actual amount in data\n");
 			exit(1);
 		}
 	}
 
-	// Initialize synthetic
-	double ascR, decR;
+	// Read, convert from arc minutes to degrees and store in R
+	float ascR, decR;
 	index = 0;
 	while (infileR >> ascR >> decR) {
 		if (index < nCoordinatePairsR * 2) {
-			h_R[index++] = ascR;
-			h_R[index++] = decR;
+			h_R[index++] = ascR * 1/60 * M_PI/180;
+			h_R[index++] = decR * 1/60 * M_PI/180;
 		} else {
 			printf("Number of coordinate pairs given in file does not match the actual amount in flat\n");
 			exit(1);
 		}
 	}
 
-	int *hostComputationResult = (int *)calloc(720, sizeof(int));
-
 	float *d_D;
-	double *d_R;
-	int *h_DD, *h_DR, *h_RR;
-	int *d_DD, *d_DR, *d_RR;
+	float *d_R;
+	unsigned long long int *h_DD, *h_DR, *h_RR;
+	unsigned long long int *d_DD, *d_DR, *d_RR;
 
-	#pragma omp parallel
-	{
-		// Device computation
-		#pragma omp single nowait
-		{
-			printf("\nRunning host side on %d threads\n", omp_get_num_threads());
+	printf("\nRunning host side on %d threads\n", omp_get_num_threads());
 
-			// Info about the GPU
-			int deviceCount;
-			cudaGetDeviceCount(&deviceCount);
-			for (int i = 0; i < deviceCount; i++) {
-				cudaDeviceProp props;
-				cudaGetDeviceProperties(&props, i);
-				printf("\nDevice nr.: %d\n", i);
-				printf("  Device name: %s\n", props.name);
-				printf("  Memory clock rate: (MHz) %lf\n", props.memoryClockRate/1000.0);
-				printf("  Memory bus width (bits): %d\n", props.memoryBusWidth);
-				printf("  Peak memory bandwith (GB/s): %f\n", 2.0*props.memoryClockRate*(props.memoryBusWidth/8)/1.0e6);
-				printf("  Compute capability: %d.%d\n", props.major, props.minor);
-				printf("  Shared memory per block: %zd\n", props.sharedMemPerBlock);
-				printf("  Multiprocessor count: %d\n\n", props.multiProcessorCount);
-			}
-
-			//cudaDeviceSetSharedMemConfig(cudaSharedMemBankSizeEightByte);
-
-			// Allocating and copying the input data to device
-			cudaMalloc((void **)&d_D, nCoordinatePairsD * 2 * sizeof(float));
-			cudaMalloc((void **)&d_R, nCoordinatePairsR * 2 * sizeof(double));
-
-			cudaMemcpy(d_D, h_D, nCoordinatePairsD * 2 * sizeof(float), cudaMemcpyHostToDevice);
-			cudaMemcpy(d_R, h_R, nCoordinatePairsR * 2 * sizeof(double), cudaMemcpyHostToDevice);
-
-			// Allocating and zero-initializing the result arrays on host
-			h_DD = (int *)calloc(720, sizeof(int));
-			h_DR = (int *)calloc(720, sizeof(int));
-			h_RR = (int *)calloc(720, sizeof(int));
-
-			// Allocating the result arrays on device
-			cudaMalloc((void **)&d_DD, 720 * sizeof(int));
-			cudaMalloc((void **)&d_DR, 720 * sizeof(int));
-			cudaMalloc((void **)&d_RR, 720 * sizeof(int));
-
-			// Copying the zero-initialized arrays to the GPU
-			cudaMemcpy(d_DD, h_DD, 720 * sizeof(int), cudaMemcpyHostToDevice);
-			cudaMemcpy(d_DR, h_DR, 720 * sizeof(int), cudaMemcpyHostToDevice);
-			cudaMemcpy(d_RR, h_RR, 720 * sizeof(int), cudaMemcpyHostToDevice);
-
-			// Calculating sizes and launching kernels
-			int blockSize = 256;
-
-			int gridSizeX = (nCoordinatePairsD + blockSize - 1) / blockSize;
-			int gridSizeY = (nCoordinatePairsR + ROWSPERTHREAD - 1) / ROWSPERTHREAD;
-			dim3 gridSize2D(gridSizeX, gridSizeY);
-			DR_kernel<<<gridSize2D, blockSize>>>(nCoordinatePairsD, nCoordinatePairsR, d_D, d_R, d_DR);
-
-			gridSizeY = (nCoordinatePairsD + ROWSPERTHREAD - 1) / ROWSPERTHREAD;
-			dim3 DDGridSize2D(gridSizeX, gridSizeY);
-			DD_kernel<<<DDGridSize2D, blockSize>>>(nCoordinatePairsD, d_D, d_DD);
-
-			// Compute RR_DEVICE_TO_HOST_RATIO of columns in RR on device
-			gridSizeX = (nCoordinatePairsR * RR_DEVICE_TO_HOST_RATIO + blockSize - 1) / blockSize;
-			gridSizeY = (nCoordinatePairsR + ROWSPERTHREAD - 1) / ROWSPERTHREAD;
-			dim3 RRGridSize2D(gridSizeX, gridSizeY);
-			RR_kernel<<<RRGridSize2D, blockSize>>>(nCoordinatePairsR * RR_DEVICE_TO_HOST_RATIO, nCoordinatePairsR, d_R, d_RR);
-		}
-
-
-		// Host computation
-
-		// Each thread has a private histogram
-		int *localHist = (int *)calloc(720, sizeof(int));
-		
-		#pragma omp for nowait
-			// Compute 1 - RR_DEVICE_TO_HOST_RATIO of columns in RR on host
-			for (int i = nCoordinatePairsR * RR_DEVICE_TO_HOST_RATIO; i < nCoordinatePairsR; i++) {
-
-				double asc1 = h_R[i * 2];
-				double dec1 = h_R[i * 2 + 1];
-
-				for (int j = i+1; j < nCoordinatePairsR; j++) {
-
-					double asc2 = h_R[j * 2];
-					double dec2 = h_R[j * 2 + 1];
-
-					if (fabs(asc1-asc2) > 0.0000001 || fabs(dec1-dec2) > 0.0000001) {
-						double decimalResult = acos(sin(dec1) * sin(dec2) + cos(dec1) * cos(dec2) * cos(asc1-asc2));
-						decimalResult *= 180/M_PI;
-						int resultIndex = floor(decimalResult/0.25);
-						if (resultIndex >= 0 || resultIndex <= 719) {
-							localHist[resultIndex]++;
-						} else {
-							printf("Index %d and %d is incorrect with result %lf\n", i, j, decimalResult);
-						}
-					}
-				}
-			}
-
-		// Each thread updates the global histogram
-		#pragma omp critical
-		{
-			for (int i = 0; i < 720; i++) {
-				hostComputationResult[i] += localHist[i];
-			}
-		}
+	// Info about the GPU
+	int deviceCount;
+	cudaGetDeviceCount(&deviceCount);
+	for (int i = 0; i < deviceCount; i++) {
+		cudaDeviceProp props;
+		cudaGetDeviceProperties(&props, i);
+		printf("\nDevice nr.: %d\n", i);
+		printf("  Device name: %s\n", props.name);
+		printf("  Memory clock rate: (MHz) %lf\n", props.memoryClockRate/1000.0);
+		printf("  Memory bus width (bits): %d\n", props.memoryBusWidth);
+		printf("  Peak memory bandwith (GB/s): %f\n", 2.0*props.memoryClockRate*(props.memoryBusWidth/8)/1.0e6);
+		printf("  Compute capability: %d.%d\n", props.major, props.minor);
+		printf("  Shared memory per block: %zd\n", props.sharedMemPerBlock);
+		printf("  Multiprocessor count: %d\n\n", props.multiProcessorCount);
 	}
+
+	// Allocating and copying the input data to device
+	cudaMalloc((void **)&d_D, nCoordinatePairsD * 2 * sizeof(float));
+	cudaMalloc((void **)&d_R, nCoordinatePairsR * 2 * sizeof(float));
+
+	cudaMemcpy(d_D, h_D, nCoordinatePairsD * 2 * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_R, h_R, nCoordinatePairsR * 2 * sizeof(float), cudaMemcpyHostToDevice);
+
+	// Allocating and zero-initializing the result arrays on host
+	h_DD = (unsigned long long int *)calloc(720, sizeof(unsigned long long int));
+	h_DR = (unsigned long long int *)calloc(720, sizeof(unsigned long long int));
+	h_RR = (unsigned long long int *)calloc(720, sizeof(unsigned long long int));
+
+	// Allocating the result arrays on device
+	cudaMalloc((void **)&d_DD, 720 * sizeof(unsigned long long int));
+	cudaMalloc((void **)&d_DR, 720 * sizeof(unsigned long long int));
+	cudaMalloc((void **)&d_RR, 720 * sizeof(unsigned long long int));
+
+	// Copying the zero-initialized arrays to the GPU
+	cudaMemcpy(d_DD, h_DD, 720 * sizeof(unsigned long long int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_DR, h_DR, 720 * sizeof(unsigned long long int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_RR, h_RR, 720 * sizeof(unsigned long long int), cudaMemcpyHostToDevice);
+
+	// Device kernel for DR
+	int gridSizeX = (nCoordinatePairsD + BLOCKSIZE - 1) / BLOCKSIZE;
+	int gridSizeY = (nCoordinatePairsR + ROWSPERTHREAD - 1) / ROWSPERTHREAD;
+	dim3 gridSize2D(gridSizeX, gridSizeY);
+	DR_kernel<<<gridSize2D, BLOCKSIZE>>>(nCoordinatePairsD, nCoordinatePairsR, d_D, d_R, d_DR);
+
+	// Device kernel for DD
+	gridSizeY = (nCoordinatePairsD + ROWSPERTHREAD - 1) / ROWSPERTHREAD;
+	dim3 DDGridSize2D(gridSizeX, gridSizeY);
+	DD_or_RR_kernel<<<DDGridSize2D, BLOCKSIZE>>>(nCoordinatePairsD, nCoordinatePairsD, d_D, d_DD);
+
+	// Device kernel for RR
+	gridSizeX = (nCoordinatePairsR + BLOCKSIZE - 1) / BLOCKSIZE;
+	gridSizeY = (nCoordinatePairsR + ROWSPERTHREAD - 1) / ROWSPERTHREAD;
+	dim3 RRGridSize2D(gridSizeX, gridSizeY);
+	DD_or_RR_kernel<<<RRGridSize2D, BLOCKSIZE>>>(nCoordinatePairsR, nCoordinatePairsR, d_R, d_RR);
 
 	// Checking for errors
 	cudaError_t errSync = cudaGetLastError();
@@ -361,14 +279,14 @@ int main(void) {
 
 	// Copying the result from device to host
 	// cudaMemcpy has an implicit barrier
-	cudaMemcpy(h_DD, d_DD, 720 * sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(h_DR, d_DR, 720 * sizeof(int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(h_RR, d_RR, 720 * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_DD, d_DD, 720 * sizeof(unsigned long long int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_DR, d_DR, 720 * sizeof(unsigned long long int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_RR, d_RR, 720 * sizeof(unsigned long long int), cudaMemcpyDeviceToHost);
 
-	// Combine device and host results for RR
-	for (int i = 0; i < 720; i++) {
-		h_RR[i] += hostComputationResult[i];
-	}
+	// Add the number of galaxies in D to the first bin in DD and respectively in R and RR since we start
+	// computing for the index + 1
+	h_DD[0] += nCoordinatePairsD;
+	h_RR[0] += nCoordinatePairsR;
 
 	printf("\n");
 	long long totalDD = 0;
@@ -380,29 +298,30 @@ int main(void) {
 		totalRR += h_RR[i];
 	}
 
-	printf("Total computations\n");
+	printf("Total count in histograms\n");
 	printf("DD: %lld\n", totalDD);
 	printf("DR: %lld\n", totalDR);
 	printf("RR: %lld\n", totalRR);
 
-	// // Computing the difference
-	// double *result;
-	// result = (double *)malloc(sizeof(double) * 720);
-	// printf("\nResult:\n");
-	// for (int i = 0; i < 720; i++) {
-	// 	if(h_RR[i] == 0) {
-	// 		result[i] = 0.0;
-	// 	} else {
-	// 		result[i] = (h_DD[i] - 2 * h_DR[i] + h_RR[i]) / (double)h_RR[i];
-	// 	}
-	// 	printf("%d: %lf\n", i, result[i]);
-	// }
+	// Computing the difference
+	double result[720];
+	printf("\nResult:\n");
+	for (int i = 0; i < 7; i++) {
+		printf("%d: DD: %llu, DR: %llu, RR: %llu\n", i, h_DD[i], h_DR[i], h_RR[i]);
+		printf("  %lf\n", (double)((double)h_DD[i] - 2 * (double)h_DR[i] + (double)h_RR[i]) / (double)h_RR[i]);
+		// if(h_RR[i] == 0) {
+		// 	result[i] = 0.0;
+		// } else {
+		// 	result[i] = (h_DD[i] - 2 * h_DR[i] + h_RR[i]) / (double)h_RR[i];
+		// }
+		// printf("%d: %lf\n", i, result[i]);
+	}
 
 	cudaFree(d_D);
 	cudaFree(d_R);
 	cudaFree(d_DD);
 	cudaFree(d_DR);
-	//cudaFree(d_RR);
+	cudaFree(d_RR);
 	free(h_D);
 	free(h_R);
 	free(h_DD);
